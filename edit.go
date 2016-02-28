eec474fa19ff9159d6481238e9fd7d04f44c1dc2
cmc <cr.rey.clement@gmail.com>
2016-02-28 20:49:54 +0100

    added Edit()

---
+++ edit.go
@@ -0,0 +1,41 @@
+package forward
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+// -----------------------------------------------------------------------------
+
+// Edit starts the user's favorite editor and returns the output.
+func Edit(prefix string) ([]byte, error) {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		return nil, fmt.Errorf("$EDITOR isn't set")
+	}
+	path, err := exec.LookPath(editor)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := ioutil.TempFile(os.TempDir(), fmt.Sprintf(".forward.%s-"))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		// NOTE: file removal might fail, not much we can do though.
+		_ = os.Remove(f.Name())
+	}()
+
+	cmd := exec.Command(path, f.Name())
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadAll(f)
+}
