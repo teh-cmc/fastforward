443be2dabcfc23707589c50b57d6fd7f27472987
cmc <cr.rey.clement@gmail.com>
2016-02-29 18:25:45 +0100

    added ExtractMessage()

--- commits.go
+++ commits.go
@@ -1,17 +1,20 @@
 package forward
 
 import (
-	"log"
 	"regexp"
 	"strconv"
 )
 
 // -----------------------------------------------------------------------------
 
-var regexExtractID *regexp.Regexp
+var (
+	regexExtractID      *regexp.Regexp
+	regexExtractMessage *regexp.Regexp
+)
 
 func init() {
-	regexExtractID = regexp.MustCompile(`\(#(\d+)\)`)
+	regexExtractID = regexp.MustCompile(`^\[forward\] (?:.+:.+) -(?: \(#(\d+)\)).*`)
+	regexExtractMessage = regexp.MustCompile(`^\[forward\] (?:.+:.+) -(?: \(#\d+\))? (.*)`)
 }
 
 // -----------------------------------------------------------------------------
@@ -19,16 +22,18 @@ func init() {
 // LatestTaskID returns the ID of the last task created.
 func LatestTaskID() (int64, error) {
 	cmd := []string{
-		"log", "--grep", `\[forward\] task:new`, "--oneline", "-n", "1",
+		"log", "--grep", `\[forward\] task:new`, "--format=%s", "-n", "1",
 	}
 	output, err := GitOutput(cmd)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 	return ExtractTaskID(output)
 }
 
 // ExtractTaskID extracts the task ID from a commit message.
+//
+// `commit` must have format `--format=%s`.
 func ExtractTaskID(commit []byte) (int64, error) {
 	sms := regexExtractID.FindSubmatch(commit)
 	if len(sms) < 2 {
@@ -39,4 +44,15 @@ func ExtractTaskID(commit []byte) (int64, error) {
 		return 0, err
 	}
 	return i, nil
+}
+
+// ExtractMessage extracts the actual message from a commit message.
+//
+// `commit` must have format `--format=%s`.
+func ExtractMessage(commit []byte) (string, error) {
+	sms := regexExtractMessage.FindSubmatch(commit)
+	if len(sms) < 2 {
+		return "", nil
+	}
+	return string(sms[1]), nil
 }
