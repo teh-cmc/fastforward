feb5e9df3809ebcf85ff9fd1e6d8c8190f6ea673
cmc <cr.rey.clement@gmail.com>
2016-03-03 12:11:20 +0100

    implemented ff init

--- commands/init.go
+++ commands/init.go
@@ -9,18 +9,37 @@ import (
 // -----------------------------------------------------------------------------
 
 // Init implements the `ff init` command.
-type Init struct {
-	branch string
-}
+type Init struct{}
 
 // NewInit returns a new Init.
-func NewInit(branch string) *Init { return &Init{branch: branch} }
+func NewInit() *Init { return &Init{} }
 
 // -----------------------------------------------------------------------------
 
+// AllowAutoPulling always returns `false`.
+func (i Init) AllowAutoPulling() bool { return false }
+
+// AllowAutoPushing always returns `true`.
+func (i Init) AllowAutoPushing() bool { return true }
+
 // Init implements the `ff init` command.
-func (i Init) Run() ([]byte, error) {
-	_, err := git.Run(git.NewCheckout(i.branch))
-	output := fmt.Sprintf("FastForward branch '%s' successfully initialized.", i.branch)
-	return []byte(output), err
+func (i Init) Run(branch string) ([]byte, error) {
+	var output []byte
+	var err error
+
+	output, err = git.Run(git.NewBranch(git.BranchTypeCurrent, branch), branch)
+	if err != nil {
+		return output, err
+	}
+	current := string(output)
+	output, err = git.Run(git.NewBranch(git.BranchTypeNew, branch), branch)
+	if err != nil {
+		return output, err
+	}
+	output, err = git.Run(git.NewBranch(git.BranchTypeSwitch, current), current)
+	if err != nil {
+		return output, err
+	}
+	output = []byte(fmt.Sprintf("branch '%s' successfully initialized.", branch))
+	return output, nil
 }
