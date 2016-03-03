4322378279d4ac80c2c0e2993a71c3222c6768c5
cmc <cr.rey.clement@gmail.com>
2016-03-03 12:10:22 +0100

    implemented git pull commands

--- git/pull.go
+++ git/pull.go
@@ -2,18 +2,24 @@ package git
 
 // -----------------------------------------------------------------------------
 
-// Pull implements the `git pull` Command.
+// Pull implements the `git pull` command.
 type Pull struct {
 	branch string
 }
 
-// NewPull returns a new Pull Command.
+// NewPull returns a new `Pull` command.
 func NewPull(branch string) *Pull { return &Pull{branch: branch} }
 
 // -----------------------------------------------------------------------------
 
-// Template always returns `nil`.
-func (p Pull) Template() []byte { return nil }
+// AllowAutoCheckout always returns true.
+func (c Pull) AllowAutoCheckout() bool { return true }
 
-// Command returns a command that pulls `p.branch`.
+// Input always returns `nil`.
+func (p Pull) Input() []byte { return nil }
+
+// Command returns a `git pull` command.
 func (p Pull) Command() []string { return []string{"pull", "--rebase", p.branch} }
+
+// Transform does nothing.
+func (p Pull) Transform(output []byte) []byte { return output }
