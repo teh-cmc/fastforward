684f685ae35f2c93a52a46fdc7660a0971b697e9
cmc <cr.rey.clement@gmail.com>
2016-03-03 07:18:11 +0100

    refactored ff init command

---
+++ commands/init.go
@@ -0,0 +1,26 @@
+package commands
+
+import "github.com/teh-cmc/fastforward/git"
+
+// -----------------------------------------------------------------------------
+
+// Init implements the `ff init` command.
+//
+// Init implements the `git.Command` interface.
+type Init struct {
+	branch string
+}
+
+// NewInit returns a new Init.
+func NewInit(branch string) *Init { return &Init{branch: branch} }
+
+// -----------------------------------------------------------------------------
+
+// Template always returns `nil`.
+func (i Init) Template() []byte { return nil }
+
+// Command returns a command that creates a new git branch.
+func (i Init) Command() []string { return []string{"checkout", "-b", i.branch} }
+
+// Init implements the `fwd init` command.
+func (i Init) Run() ([]byte, error) { return git.Run(i) }
