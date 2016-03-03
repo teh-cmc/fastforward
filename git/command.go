d93efcacf4aa2fde5461105998ba1879450fa157
cmc <cr.rey.clement@gmail.com>
2016-03-03 12:09:35 +0100

    refactored git subsystem

--- git/command.go
+++ git/command.go
@@ -2,32 +2,51 @@ package git
 
 import (
 	"bytes"
+	"log"
 	"os"
 	"os/exec"
 )
 
 // -----------------------------------------------------------------------------
 
-// Command exposes methods to run a git command.
+// Command exposes methods to run a `git` command.
 type Command interface {
-	Template() []byte
+	AllowAutoCheckout() bool
+	Input() []byte
 	Command() []string
+	Transform([]byte) []byte
 }
 
+// -----------------------------------------------------------------------------
+
 // Run runs the given command `c`.
-func Run(c Command) ([]byte, error) {
+func Run(c Command, branch string) ([]byte, error) {
 	bin, err := exec.LookPath("git")
 	if err != nil {
 		return nil, err
 	}
 
-	cmd := exec.Command(bin, c.Command()...)
-	cmd.Stderr = os.Stderr
+	if c.AllowAutoCheckout() {
+		current, err := Run(NewBranch(BranchTypeCurrent, branch), branch)
+		if err != nil {
+			return nil, err
+		}
+		if _, err = Run(NewBranch(BranchTypeSwitch, branch), branch); err != nil {
+			return nil, err
+		}
+		defer func() {
+			if _, err := Run(NewBranch(BranchTypeSwitch, string(current)), branch); err != nil {
+				log.Fatal(err)
+			}
+		}()
+	}
 
+	cmd := exec.Command(bin, c.Command()...)
 	output := bytes.NewBufferString("")
+	cmd.Stderr = os.Stderr
 	cmd.Stdout = output
 
-	if input := c.Template(); input != nil {
+	if input := c.Input(); input != nil {
 		in, err := cmd.StdinPipe()
 		if err != nil {
 			return nil, err
@@ -50,5 +69,5 @@ func Run(c Command) ([]byte, error) {
 		}
 	}
 
-	return output.Bytes(), nil
+	return c.Transform(output.Bytes()), nil
 }
