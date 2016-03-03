424e8b9013b377d472b6e09fea6a16abc8436e10
cmc <cr.rey.clement@gmail.com>
2016-03-03 12:11:38 +0100

    implemented ff push

--- commands/push.go
+++ commands/push.go
@@ -5,14 +5,20 @@ import "github.com/teh-cmc/fastforward/git"
 // -----------------------------------------------------------------------------
 
 // Push implements the `ff push` command.
-type Push struct {
-	branch string
-}
+type Push struct{}
 
 // NewPush returns a new Push.
-func NewPush(branch string) *Push { return &Push{branch: branch} }
+func NewPush() *Push { return &Push{} }
 
 // -----------------------------------------------------------------------------
 
+// AllowAutoPulling always returns `false`.
+func (p Push) AllowAutoPulling() bool { return false }
+
+// AllowAutoPushing always returns `false`.
+func (p Push) AllowAutoPushing() bool { return false }
+
 // Push implements the `ff push` command.
-func (p Push) Run() ([]byte, error) { return git.Run(git.NewPush(p.branch)) }
+func (p Push) Run(branch string) ([]byte, error) {
+	return git.Run(git.NewPush(branch), branch)
+}
